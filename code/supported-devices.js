da5729ff4185251b5655fa5bd9714490a22b19cc
Hideyoshi Moriya <hide@piyo.jp>
2025-01-26 22:17:31 +0900

    rename productName to product

--- code/supported-devices.js
+++ code/supported-devices.js
@@ -2,12 +2,12 @@ const supportedDevices = {
     g29 : {
         vendorId: 1133,
         productId: 49743,
-        productName: 'G29 Driving Force Racing Wheel'
+        product: 'G29 Driving Force Racing Wheel'
     },
     g923 : {
         vendorId: 1133,
         productId: 49766,
-        productName: 'G923 Racing Wheel for PlayStation and PC'
+        product: 'G923 Racing Wheel for PlayStation and PC'
     },
 }
 
